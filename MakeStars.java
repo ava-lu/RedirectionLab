0fd71bf170efa59e0735867585300bef54cf24ad
ava-lu <alu21@stuy.edu>
2021-01-04 17:29:40 -0500

    finished MakeStars and tested

--- MakeStars.java
+++ MakeStars.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class MakeStars {
   public static void main(String[] args) {
-    
+    Scanner file = new Scanner(System.in);
+    while (file.hasNextLine()) {
+      Scanner line = new Scanner(file.nextLine());
+      while (line.hasNext()) {
+        String word = line.next();
+        for (int i=0; i<word.length();i++) {
+          System.out.print("*");
+        }
+        System.out.print(" ");
+      }
+      System.out.print("\n");
+    }
   }
+
 }
